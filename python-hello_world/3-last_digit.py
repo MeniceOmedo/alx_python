3c1070da6f9802d5b93b0eb5e4541621ca35a874
Menice Omedo <menicebeccah@gmail.com>
2023-07-26 20:08:27 +0300

    added num<0

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,14 +1,22 @@
 import random
 number= random.randint(-10000,10000)
-number=str(number)
-last_digit=number[-1]
-if( int(last_digit)>5):
-    print("last digit of {} is {} and is greater than 5".format(number,last_digit))
-elif(int( last_digit)==0):
-    print("last digit of {} is {} and is 0".format(number,last_digit))
-elif( (int(last_digit)<6) and   (int (last_digit)!=0)) :
-    print("last digit of {} is {} and is less than 6 and not 0".format(number,last_digit)) 
-else:
-    print("in else block")
+number=int(number)
+last_digit=(abs(number))%10
+if number<0:
+     last_digit=last_digit*-1
+     print=("last digit of {} is {} and is less than 0".format(number,last_digit))
+elif number>0:     
 
-print()
+    if last_digit>5:
+         print("last digit of {} is {} and is greater than 5".format(number,last_digit))
+    elif last_digit==0:
+        print("last digit of {} is {} and is 0".format(number,last_digit))
+    elif( last_digit<6) and   (last_digit!=0) :
+        print("last digit of {} is {} and is less than 6 and not 0".format(number,last_digit)) 
+    #elif(int((number<0))):
+       #last_digit=(last_digit)*-1
+       #print("last digit of {} is {} and is less than 0".format(number,last_digit))        
+
+else:
+         print("in else block")
+
