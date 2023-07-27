a64bdb40c4e4d4b360994826065556b6d4d63698
Menice Omedo <menicebeccah@gmail.com>
2023-07-27 17:07:11 +0300

    done corrections

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,22 +1,19 @@
 import random
 number= random.randint(-10000,10000)
-number=int(number)
+
 last_digit=(abs(number))%10
 if number<0:
-     last_digit=last_digit*-1
-     print=("last digit of {} is {} and is less than 0".format(number,last_digit))
-elif number>0:     
-
+    last_digit=last_digit*-1
+    print("last digit of {} is {} and is less than 0".format(number,last_digit))
+         
+if number>0:
     if last_digit>5:
-         print("last digit of {} is {} and is greater than 5".format(number,last_digit))
+        print("Last digit of {} is {} and is greater than 5".format(number,last_digit))
+
     elif last_digit==0:
-        print("last digit of {} is {} and is 0".format(number,last_digit))
-    elif( last_digit<6) and   (last_digit!=0) :
-        print("last digit of {} is {} and is less than 6 and not 0".format(number,last_digit)) 
-    #elif(int((number<0))):
-       #last_digit=(last_digit)*-1
-       #print("last digit of {} is {} and is le        
-
-else:
-         print("in else block")
-
+        print("Last digit of {} is {} and is zero".format(number,last_digit))
+    elif (last_digit<6) and (last_digit!=0):
+        print("last digit of {} is {} and is less than 6 and not 0".format(number,last_digit))
+    else:
+        print()    
+   
