cb7df5ac0eb5b86970b47d6002c027eaad4f5be7
MeniceOmedo <104766121+MeniceOmedo@users.noreply.github.com>
2023-07-27 17:02:17 +0300

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,14 +1,1 @@
-import random
-number= random.randint(-10000,10000)
-number=str(number)
-last_digit=number[-1]
-if( int(last_digit)>5):
-    print("last digit of {} is {} and is greater than 5".format(number,last_digit))
-elif(int( last_digit)==0):
-    print("last digit of {} is {} and is 0".format(number,last_digit))
-elif( (int(last_digit)<6) and   (int (last_digit)!=0)) :
-    print("last digit of {} is {} and is less than 6 and not 0".format(number,last_digit)) 
-else:
-    print("in else block")
 
-print()
